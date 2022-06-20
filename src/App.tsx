ceaf93a78a53a8e67d459b0cc60e1dbb39eba311
한태경 <roenh@statnco.com>
2022-06-20 17:11:58 +0900

    라우터 및 emotion 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import loadable from '@loadable/component'
+
+
+const Login = loadable(() => import('./pages/Login'))
+const Signup = loadable(() => import('./pages/Signup'))
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<Login/>}></Route>
+      <Route path='/login' element={<Login/>}></Route>
+      <Route path='/signup' element={<Signup/>}></Route>
+      <Route path='/workspace/:workspace/*' element={<Login/>}></Route>
+    </Routes>
   );
 }
 
